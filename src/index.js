8721c81bf32e0010d34ec1713187284e3874814e
Pierre Pillot <pierrepillot0110@gmail.com>
2023-05-26 22:08:10 +0200

    routes configurées

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './pages/home/index';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import Survey from './pages/survey';
+import Header from './components/Header/index'
+import Error from './components/Error';
+ 
+ReactDOM.render(
+    <React.StrictMode>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/survey" element={<Survey />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Router>
+    </React.StrictMode>,
+document.getElementById('root')
+)
