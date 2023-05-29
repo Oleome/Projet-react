f54bb1d40fa8c24dfcccd1bc6def417ff6cf2dff
Pierre Pillot <pierrepillot0110@gmail.com>
2023-05-29 22:29:49 +0200

    Footer ok

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,13 +3,30 @@ import logfoot from '../../assets/logfoot.png'
 
 const StyledFooter = styled.footer`
     background-color: black;
+    height: 209px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
+
+const StyledLogo = styled.img `
+    width: 122px;
+    height: 35px;
+    margin-top: 66px;
+`
+
+const StyledSpan = styled.span`
+    color: white;
+    margin-top: 40px;
+`
+
 
 
 function Footer() {
     return (
         <StyledFooter>
-            <img src={logfoot} alt="logo de kasa" />
+            <StyledLogo src={logfoot} alt="logo de kasa" />
+            <StyledSpan>"© 2020 Kasa. All rights reserved"</StyledSpan>
         </StyledFooter>
     )
 }
