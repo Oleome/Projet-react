5e95825fbcdae6735286c5c61cd8853fcbbcd24f
Pierre Pillot <pierrepillot0110@gmail.com>
2023-06-03 15:06:51 +0200

    error.scss

--- src/pages/Fiche-logement.jsx
+++ src/pages/Fiche-logement.jsx
@@ -1,16 +1,8 @@
-import styled from 'styled-components';
-
-const StyledContainer = styled.div`
-  padding: 150px;
-  background-color: #E5E5E5;
-  min-width: 300px;
-`
-
 function FicheLogement () {
     return (
-        <StyledContainer>
+        <div>
           <h2>Fiche logement</h2>
-        </StyledContainer>
+        </div>
       )}
     
 export default FicheLogement;
--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,52 +1,13 @@
 import errorpic from '../assets/errorpic.png'
-import styled from 'styled-components'
-
-const StyledDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #E5E5E5;
-    min-width: 300px;
-`
-
-const StyledImg = styled.img`
-    width:40%;
-    height:auto;
-    margin: 139px 0;
-`
-
-const StyledSpan = styled.span`
-    text-align: center;
-    margin: 0 184px;
-    font-size: 36px;
-    @media screen and (max-width: 960px) {
-        font-size: 28px;
-        margin: 0 114px;
-    }
-    @media screen and (max-width: 650px) {
-        font-size: 18px;
-        margin: 0 57.5px;
-    }
-`
-
-const StyledLink = styled.a`
-    margin: 139px 0;
-    font-size: 18px;
-    @media screen and (max-width: 960px) {
-        font-size: 16px;
-    }
-    @media screen and (max-width: 650px) {
-        font-size: 14px;
-    }
-`
+import '../styles/error.scss'
 
 function Error() {
     return (
-        <StyledDiv>
-            <StyledImg src={errorpic} alt="erreur 404" />
-            <StyledSpan>Oups! La page que vous demandez n'existe pas.</StyledSpan>
-            <StyledLink href="/">Retourner sur la page d’accueil</StyledLink>
-        </StyledDiv>
+        <div className='error__div'>
+            <img src={errorpic} alt="erreur 404" />
+            <span>Oups! La page que vous demandez n'existe pas.</span>
+            <a href="/">Retourner sur la page d’accueil</a>
+        </div>
     )
 }
  
