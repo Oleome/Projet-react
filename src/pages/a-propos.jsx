21704e0c9b62fe5d39c312d870aa1a74028c17fe
Pierre Pillot <pierrepillot0110@gmail.com>
2023-06-03 23:03:34 +0200

    wip a-propos

--- src/pages/a-propos.jsx
+++ src/pages/a-propos.jsx
@@ -1,28 +1,17 @@
-import styled from 'styled-components'
-
-
-const StyledContainer = styled.div`
-    padding: 150px;
-    background-color: #E5E5E5;
-    min-width: 300px
-`
+import '../styles/a-propos.scss'
+import arrow_up from '../assets/arrow_up.png'
+import arrow_down from '../assets/arrow_up.png'
+import { useState } from 'react'
 
 function APropos() {
-    return (
-        <StyledContainer>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem sit explicabo obcaecati, aspernatur saepe, consequuntur ex, voluptatem natus quibusdam odit maiores quo assumenda placeat deserunt ipsa minima velit alias hic.
-            A saepe facilis quasi, minus consectetur harum ea ipsum ut repudiandae distinctio esse eum adipisci ab ad magni reprehenderit quia quaerat, ex soluta amet voluptate ipsa? Voluptate pariatur earum cupiditate.
-            Corporis, nostrum aut asperiores debitis eos excepturi magni consectetur assumenda hic. Aliquam atque inventore maiores beatae porro enim, et voluptates officia facilis a asperiores! Aspernatur quis fuga iste eligendi animi!
-            Esse, maxime! Tempora dolore esse quaerat aut delectus eveniet possimus reiciendis, ex autem non corporis similique officiis nemo aspernatur consequuntur nihil, eaque ad reprehenderit? Exercitationem dolore repellat harum molestiae assumenda.
-            Ipsa ipsum at est rerum deserunt, odit veritatis alias mollitia corporis tempore aspernatur quod dicta nobis architecto. Dolor, ullam illum accusamus aspernatur voluptatibus mollitia ab recusandae ducimus in perferendis deserunt!
-            Rem cumque molestias molestiae, optio impedit commodi quos minima autem assumenda? Vitae laboriosam repellendus esse voluptatibus odit incidunt iusto, aliquid a architecto nisi commodi necessitatibus corporis voluptates dignissimos maiores quos!
-            Corrupti non libero et, accusamus saepe incidunt, perspiciatis eveniet sed id enim perferendis, reprehenderit placeat numquam quibusdam? Nulla ex inventore, illum dolor, modi quia culpa asperiores perferendis ratione nobis quos.
-            Voluptate in explicabo ducimus dolorum est quo dignissimos, molestiae amet laudantium fugiat optio voluptates itaque sequi praesentium perspiciatis expedita repudiandae suscipit veritatis repellat cupiditate. Pariatur iste maxime doloribus? Ullam, quasi.
-            Nihil quod ab ullam error nemo fugit aliquid iure temporibus ipsam debitis, a, assumenda, tempora maiores sint necessitatibus et? Dicta necessitatibus dolor voluptatibus corrupti sed sint animi voluptate explicabo maiores.
-            Mollitia, nulla ipsam! Rerum error itaque impedit, illo quam est eligendi ea quisquam, voluptatibus nobis quis molestias ad, a quas hic aspernatur. Ad exercitationem, eos incidunt voluptas modi nostrum quis!
-            Quae, qui. Incidunt eaque vel sed officia sapiente! Necessitatibus ipsam asperiores beatae voluptates eum! Perspiciatis, nam atque aspernatur dolorum, dolore nobis omnis aperiam suscipit repudiandae voluptatum et iusto eligendi! Omnis?
-            Tempore maxime, officia voluptates aperiam hic omnis doloribus eum, commodi illo ratione rerum optio enim officiis quis rem nam provident sequi laboriosam magnam quos sint facere id totam laudantium. Ratione?</p>            
-        </StyledContainer>
+    const [isOpen, setIsOpen] = useState(false)
+
+    return isOpen ? (
+        <div className="a-propos-div">
+            <img onClick={() => setIsOpen(false)} src={arrow_up} alt="une flèche vers le haut" />
+        </div>
+    ) : (
+        <img onClick={() => setIsOpen(true)} src={arrow_down} alt="une flèche vers le bas" />
     )
 }
 
