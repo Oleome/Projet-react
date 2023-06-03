36e1ab08084dc32f9fca2555c7f435286457b3fc
Pierre Pillot <pierrepillot0110@gmail.com>
2023-06-03 15:28:16 +0200

    modifs mineurs

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,17 @@
 import Cards from '../components/Cards/index'
+import '../data/backend.json'
+
+
 
 function App() {
   return (
     <>
-      <Cards />
+      <Cards 
+      />
       <h2>Home</h2>
     </>
   )}
 
 export default App;
+
+
