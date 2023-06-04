d4cec0ab8a98156b6484e04e2e7fdb9357a04499
Pierre Pillot <pierrepillot0110@gmail.com>
2023-06-04 22:15:24 +0200

    fin de soirée

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,7 +9,15 @@ function App() {
     <main>
       <img className='container1' src={fond_mer} alt="" />
       <div className='container2'>
-        
+        {jsonData.map((item) => (
+          <Card
+              key={item.id}
+              description={item.description}
+              cover={item.cover}
+              title={item.title}
+              tags={item.tags}
+          />
+        ))}
       </div>
     </main>
   )}
