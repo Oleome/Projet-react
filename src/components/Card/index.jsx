bd65cabec162ecaa074746d4241efd22cb059cff
Bilou <pierrepillot0110@gmail.com>
2023-06-07 09:28:30 +0200

    wip

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,12 @@
 import '../../styles/card.scss'
 import arrow_up from '../../assets/arrow_up.png'
-import arrow_down from '../../assets/arrow_up.png'
+import arrow_down from '../../assets/arrow_down.png'
 import { useState } from 'react'
 
 function Card({ title, description, cover, tags, location, equipments, rating }) {
 
     const [isOpen, setIsOpen] = useState(false)
-  
+    
 
     return (
         <div className="card">
@@ -24,10 +24,10 @@ function Card({ title, description, cover, tags, location, equipments, rating })
                     <div className='card__description--container'>
                         <button className='card__description--button' onClick={() => setIsOpen(false)}><img className='arrow-up' src={arrow_up} alt="une flèche vers le haut" /></button>
                     </div>
-                    <p className='description'>{description}</p>
+                    <p className='description fade-in'>{description}</p>
                 </div>
             ) : (
-                <div className='card__description--container'>
+                <div className='card__description--container fade-out'>
                     <button onClick={() => setIsOpen(true)}><img className='arrow-down' src={arrow_down} alt="une flèche vers le bas" /></button>
                 </div>
             )}
