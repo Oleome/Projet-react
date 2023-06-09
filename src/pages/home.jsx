c837042d9178cd173c3fdf556a34c79003d4c9ad
Pierre Pillot <pierrepillot0110@gmail.com>
2023-06-09 22:55:52 +0200

    composant card

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,12 @@
 import jsonData from '../data/backend.json'
 import '../styles/home.scss'
 import Banner from '../components/Banner'
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import fond_mer from '../assets/fond_mer.png'
 
 
 function App() {
 
-  let navigate = useNavigate();
 
   return (
     <main>
@@ -15,13 +14,14 @@ function App() {
         {fond_mer}
       </Banner>
       <section className='container2'>
-      {jsonData.map((item) => (
-        <div onClick={() => navigate('/fiche_logement/'+item.id)} className='container2__title' key={item.id}>
-          <span>{item.title}</span>
-        </div>
-      )
-        
-      )}
+        {jsonData.map((item) => (
+          <Link to={`/fiche_logement/${item.id}`} key={item.id} className='container2__card'>
+            <img src={item.cover} alt="cover" />
+            <div className='container2__title'>
+              <span>{item.title}</span>
+            </div>
+          </Link>
+        ))}
       </section>
     </main>
   )}
