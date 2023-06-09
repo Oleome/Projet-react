be91117ace05aea71e404360cd3840da2aac2c47
xurei <xureilab@gmail.com>
2023-06-09 18:20:32 +0200

    Help for Collapse

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -8,15 +8,15 @@ function Collapse({children, title}) {
     const [isOpen, setIsOpen] = useState(false)
 
     return (
-        <>
-            <div className='collapse__container'>
+        <div className='collapse__container'>
+            <div className='collapse__header' onClick={() => setIsOpen(!isOpen)}>
                 <span className='collapse__title'>{title}</span>
-                <button className='collapse__button' onClick={() => setIsOpen(!isOpen)}>
+                <button className='collapse__button'>
                     <img className={`arrow ${isOpen ? 'arrow-down' : 'arrow-up'}`} src={isOpen ? arrow_down : arrow_up} alt="flèche" />
                 </button>
             </div>
             <div className={`collapse ${isOpen ? 'collapse-open' : 'collapse-close'}`}>{children}</div>
-        </>
+        </div>
     )
 }
 
