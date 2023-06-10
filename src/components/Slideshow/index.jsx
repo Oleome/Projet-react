257ce8217391f1bbecf95e94eecd28fba85fcd96
Pierre Pillot <pierrepillot0110@gmail.com>
2023-06-10 11:23:39 +0200

    wip

--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -1,20 +1,25 @@
 import jsonData from '../../data/backend.json'
+import arrow_left from '../../assets/arrow_left.png'
+import arrow_right from '../../assets/arrow_right.png'
 
-function SlideSwow () {
 
-    const tabImg = jsonData.pictures.Arrayfrom();
-   
-    
 
+function SlideSwow ({children, counter, tableauImage}) {
+
+tableauImage = []
 
     return (
-        (tabImg.length > 1) ? {
-            
-        }
-
-        : {
-
-        }
+        <div className=''>
+            <div className='' >
+                <span className=''>1/${tableauImage.length}</span>
+                <button className='' onClick={() => }>
+                    <img src={arrow_right} alt="flèche allant à droite" />
+                </button>
+                <button className='' onClick={() => }>
+                    <img src={arrow_left} alt="flèche allant à gauche" />
+                </button>
+            </div>
+        </div>
     )
 }
 
