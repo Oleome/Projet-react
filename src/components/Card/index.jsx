cc9b88b9388129213ad352d38646a930f39aba30
Pierre Pillot <pierrepillot0110@gmail.com>
2023-06-10 09:56:13 +0200

    composant card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,19 @@
+import jsonData from '../../data/backend.json'
+import { Link } from 'react-router-dom'
+import '../../styles/home.scss'
+
 function Card() {
     return (
-
+        <div>
+            {jsonData.map((item) => (
+                <Link to={`/fiche_logement/${item.id}`} key={item.id} className='container2__card'>
+                <img src={item.cover} alt="cover" />
+                <div className='container2__title'>
+                    <span>{item.title}</span>
+                </div>
+                </Link>
+            ))}
+        </div>
     )
 } 
 
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,7 @@
-import jsonData from '../data/backend.json'
 import '../styles/home.scss'
 import Banner from '../components/Banner'
-import { Link } from 'react-router-dom'
 import fond_mer from '../assets/fond_mer.png'
+import Card from '../components/Card/index'
 
 
 function App() {
@@ -14,14 +13,7 @@ function App() {
         {fond_mer}
       </Banner>
       <section className='container2'>
-        {jsonData.map((item) => (
-          <Link to={`/fiche_logement/${item.id}`} key={item.id} className='container2__card'>
-            <img src={item.cover} alt="cover" />
-            <div className='container2__title'>
-              <span>{item.title}</span>
-            </div>
-          </Link>
-        ))}
+        <Card />
       </section>
     </main>
   )}
