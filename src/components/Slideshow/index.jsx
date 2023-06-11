68db2ab8e9be3197d02c8703354d95e93a0fef97
Pierre Pillot <pierrepillot0110@gmail.com>
2023-06-11 13:35:47 +0200

    comportement si une seule picture ok

--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -10,27 +10,31 @@ function SlideShow ({pictures, index}) {
 const [currentIndex, setCurrentIndex] = useState(0)
 
 const goToNextImage = () => {
-    const isFirstSlide = currentIndex === 0 ;
-    const newIndex = isFirstSlide ? pictures.length - 1 : currentIndex - 1 ;
+    const newIndex = (currentIndex + 1) % pictures.length; 
     setCurrentIndex(newIndex);
-};
+  };
 
-const goToPreviousImage = () => {
-    const isLastSlide = currentIndex === pictures.length - 1 ;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1 ;
+  const goToPreviousImage = () => {
+    const newIndex = (currentIndex - 1 + pictures.length) % pictures.length;
     setCurrentIndex(newIndex);
-};
+  };
 
-    return (
+    return   ( 
         <section>
             <div className='slideshow' >
-                <span className='slideshow__count'>{`${currentIndex + 1}/${pictures.length}`}</span>
-                <button className='slideshow__right-arrow' onClick={goToNextImage}>
-                    <img src={arrow_right} alt="flèche allant à droite" />
-                </button>
-                <button className='slideshow__left-arrow' onClick={goToPreviousImage}>
-                    <img src={arrow_left} alt="flèche allant à gauche" />
-                </button>
+                { pictures.length > 1 ? ( 
+                    <>
+                        <span className='slideshow__count'>{`${currentIndex + 1}/${pictures.length}`}</span> 
+                        <button className='slideshow__right-arrow' onClick={goToNextImage}>
+                            <img src={arrow_right} alt="flèche allant à droite" />
+                        </button>
+                        <button className='slideshow__left-arrow' onClick={goToPreviousImage}>
+                            <img src={arrow_left} alt="flèche allant à gauche" />
+                        </button>
+                    </>
+                ) : ( 
+                null 
+                )}
                 <img className='slideshow__picture' key={`${pictures}-${index}`} src={pictures[currentIndex]} alt="" />                
             </div>
         </section>
