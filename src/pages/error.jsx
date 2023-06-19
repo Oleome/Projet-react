a8c153af7563385b0cd6899d6baf568a231f6f78
Pierre Pillot <pierrepillot0110@gmail.com>
2023-06-19 13:59:38 +0200

    corrections soutenance

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -6,7 +6,7 @@ function Error() {
         <div className='error__div'>
             <img src={errorpic} alt="erreur 404" />
             <span>Oups! La page que vous demandez n'existe pas.</span>
-            <a href="/">Retourner sur la page d’accueil</a>
+            <a href="/">Retourner sur la page d'accueil</a>
         </div>
     )
 }
