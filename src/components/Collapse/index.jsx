05ad1c6b6969e21c5af6024bdabcae47c9a265a7
Bilou <pierrepillot0110@gmail.com>
2023-06-21 20:38:03 +0200

    version corrigée

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -5,17 +5,31 @@ import { useState } from 'react'
 
 function Collapse({children, title}) {
 
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
+    const [isClosing, setIsClosing] = useState(false);
+
 
     return (
         <div className={'collapse__container'}>
-            <div className='collapse__header' onClick={() => setIsOpen(!isOpen)}>
+            <div className='collapse__header' onClick={() => {
+                    if(!isOpen) {
+                        setIsOpen(true);
+                    } 
+                    else {
+                        setIsClosing(true);
+                        setTimeout(() => {
+                            setIsOpen(false);
+                            setIsClosing(false);
+                        }, 500)  
+                    }
+                } 
+            }>
                 <span className='collapse__title'>{title}</span>
                 <button className='collapse__button'>
                     <img className={`arrow ${isOpen ? 'arrow-down' : 'arrow-up'}`} src={isOpen ? arrow_down : arrow_up} alt="flèche" />
                 </button>
             </div>
-            <div className={`collapse ${isOpen ? 'collapse-open' : 'collapse-close'}`}>{children}</div>
+            <div className={`collapse ${isOpen ? 'collapse-open' : 'collapse-close'} ${isClosing ? 'collapse-closing' : ''}`}>{children}</div>
         </div>
     )
 }
